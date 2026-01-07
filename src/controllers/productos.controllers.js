bbce016b55927ae0e78569863cc1bbd61973f202
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-01-07 19:08:34 -0300

    agregue a la logica el crear, listar y obtener por ID

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto.js";
+
 export const prueba = (req, res) => {
     console.log('desde el controlador de prueba');
     res.send('Prueba desde el controlador');
@@ -6,14 +8,47 @@ export const prueba = (req, res) => {
 
 export const crearProducto = async (req, res) => {
     //enviamos una respuesta
-    res.send('Aqui tenemos que crear el producto')
+    //res.send('Aqui tenemos que crear el producto')
 
     try {
+        console.log(req);
         
+     const productoNuevo = new Producto(req.body);
+         
+     await productoNuevo.save();
+    
+     res.status(201).json({mensaje: 'El producto fue creado exitosamente'})
+
     } catch (error) {
         console.error(error);
         //error interno del server
         res.status(500).json({mensaje: 'Ocurrio un error al crear el producto'})
     }
 
+}
+
+export const listarProductos = async (req, res) => {
+    try {
+        const productos = await Producto.find();
+        res.status(200).json(productos)
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({mensaje: "Ocurrio un error al listar los productos"})
+    }
+}
+
+export const obtenerProducto = async (req, res) => {
+    try {
+        console.log(req.params);
+        const productoBuscado = await Producto.findById(req.params.id);
+        if(!productoBuscado) {
+            //404 not found
+            return res.status(404).json({mensaje: 'No se encontro el producto'})
+        }
+        res.status(200).json(productoBuscado)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({mensaje: 'Ocurrio un error al listar el producto'})
+    }
 }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { crearProducto, prueba } from "../controllers/productos.controllers.js";
+import { crearProducto, listarProductos, obtenerProducto, prueba } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route('/test').get(prueba);
 
-router.route('/').post(crearProducto);
+router.route('/').post(crearProducto).get(listarProductos);
+
+router.route('/:id').get(obtenerProducto)
 
 export default router;
