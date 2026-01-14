483bfc1fea5073f123794df9007031fa2a58d7f7
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-01-14 19:09:58 -0300

    agregue resultadoValidacion

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { crearProducto, prueba } from "../controllers/productos.controllers.js";
+import { borrarProductoPorID, crearProducto, editarProductoPorID, listarProductos, obtenerProducto, prueba } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
 router.route('/test').get(prueba);
 
-router.route('/').post(crearProducto);
+router.route('/').post(crearProducto).get(listarProductos);
+
+router.route('/:id').get(obtenerProducto).delete(borrarProductoPorID).put(editarProductoPorID)
 
 export default router;
