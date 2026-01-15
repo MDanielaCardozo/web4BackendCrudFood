69dc024bd638be0f53b87549140c0e9f44b6a63d
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-01-14 21:43:23 -0300

    agregue validaciones y el modelo del usuario

---
+++ src/middlewares/validacionProducto.js
@@ -0,0 +1,67 @@
+import { body } from "express-validator";
+import resultadoValidacion from "./resultadoValidacion.js"
+import Producto from "../models/producto.js";
+
+const validacionProducto = [
+    //invocamos al body(chequeamos que hay en el body de la solicitud)
+    body("nombreProducto")
+    //metodos de express-validator
+    .notEmpty()
+    .withMessage("El nombre es un dato obligatorio")
+    .isLength({ min: 2, max: 100})
+    .withMessage("El nombre del producto debe tener entre 2 y 100 caracteres")
+    .custom(async (valor, {req}) => {
+        const productoExistente = await Producto.findOne({
+            nombreProducto: valor,
+        })
+        if(!productoExistente){
+            return true;
+        }
+        //chequear si el ID del PUT ya tenga ese nombre en otro producto
+        if(
+            req.params?.id && 
+            productoExistente._id.toString() === req.params.id
+        ){
+            return true;
+        }
+        //enviar un msj de error
+        throw new Error("Ya existe un producto con ese nombre")
+    }),
+    body("precio")
+    .notEmpty()
+    .withMessage("El precio del producto es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio del producto debe ser numerico")
+    .isFloat({ min: 100, max: 1000000 })
+    .withMessage(
+      "El precio del producto debe estar entre 100 y 1000000 de pesos argentinos"
+    ),
+    body("descripcion_breve")
+    .notEmpty()
+    .withMessage("La descripcion breve es un dato obligatorio")
+    .isLength({ min: 5, max: 250 })
+    .withMessage("La descripcion breve debe tener entre 5 y 250 caracteres"),
+    body("descripcion_amplia")
+    .notEmpty()
+    .withMessage("La descripcion amplia es un dato obligatorio")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripcion amplia debe tener entre 10 y 500 caracteres"),
+    body("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isIn([
+        "Acompañamientos",
+      "Bebidas",
+      "Ensaladas",
+      "Hamburguesas",
+      "Postres",
+      "Pizzas",
+      "Sándwiches y Wraps",
+      "Veggie/Veganas",
+    ])
+    .withMessage(
+      'La categoria debe ser uno de los siguientes terminos: ["Acompañamientos", "Bebidas", "Ensaladas", "Hamburguesas","Postres", "Pizzas", "Sándwiches y Wraps", "Veggie/Veganas"]'
+    ),
+    (req, res, next) => resultadoValidacion(req, res, next),
+];
+export default validacionProducto;
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { borrarProductoPorID, crearProducto, editarProductoPorID, listarProductos, obtenerProducto, prueba } from "../controllers/productos.controllers.js";
+import validacionProducto from "../middlewares/validacionProducto.js"
+import validacionIdProducto from "../middlewares/validacionIdProducto.js";
 
 const router = Router();
 
 router.route('/test').get(prueba);
 
-router.route('/').post(crearProducto).get(listarProductos);
+router.route('/').post(validacionProducto, crearProducto).get(listarProductos);
 
-router.route('/:id').get(obtenerProducto).delete(borrarProductoPorID).put(editarProductoPorID)
+router.route('/:id').get(validacionIdProducto, obtenerProducto).delete(validacionIdProducto, borrarProductoPorID).put([validacionIdProducto, validacionProducto], editarProductoPorID)
 
 export default router;
