da6b410aa222186e4f7386d1b2c5abd883d30da1
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-01-19 15:09:07 -0300

    agregue rutas

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { borrarProductoPorID, crearProducto, editarProductoPorID, listarProductos, obtenerProducto, prueba } from "../controllers/productos.controllers.js";
 import validacionProducto from "../middlewares/validacionProducto.js"
 import validacionIdProducto from "../middlewares/validacionIdProducto.js";
+import verificarJWT from "../middlewares/verificarToken.js"
 
 const router = Router();
 
 router.route('/test').get(prueba);
 
-router.route('/').post(validacionProducto, crearProducto).get(listarProductos);
+router.route('/').post([verificarJWT, validacionProducto], crearProducto).get(listarProductos);
 
-router.route('/:id').get(validacionIdProducto, obtenerProducto).delete(validacionIdProducto, borrarProductoPorID).put([validacionIdProducto, validacionProducto], editarProductoPorID)
+router.route('/:id').get(validacionIdProducto, obtenerProducto).delete([verificarJWT, validacionIdProducto], borrarProductoPorID).put([verificarJWT, validacionIdProducto, validacionProducto], editarProductoPorID)
 
 export default router;
